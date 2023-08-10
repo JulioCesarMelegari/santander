fd5f695b00c8960ec95697493e318d7672e7bbf2
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2023-08-09 21:31:50 -0300

    card dinamic

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/AccountDataModel';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
+export class CardBoxComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  accountData: AccountDataModel = {
+    name:"Zico da Silva",
+    account:{
+      agency:"4589",
+      number:"45369-6"
+    },
+    card:{
+      limit: 1000,
+      number:"456"
+    }
+  };
 
 }
